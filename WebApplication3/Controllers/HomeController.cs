e2465b5a08fd306fe28d9739ab4c7ca15125e866
nnmedrano <nnmedrano94@hotmail.com>
2017-12-22 03:48:42 -0300

    Vistas

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Kitchen()
+        {
+            return View();
+        }
+
+        public ActionResult Living()
+        {
+            return View();
+        }
+
+        public ActionResult Bathroom()
+        {
+            return View();
+        }
+
+        public ActionResult Bedroom()
+        {
+            return View();
+        }
+    }
+}
